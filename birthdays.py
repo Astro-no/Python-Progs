84823636960f2a62d596266570d3bd6a3de32b6b
Job Njenga <90536181+Astro-no@users.noreply.github.com>
2024-01-07 16:05:16 +0000

    Add files via upload

---
+++ birthdays.py
@@ -0,0 +1,14 @@
+birthdays = {'Job': 'Aug 5', 'Ric': 'June 14', 'Chris': 'Sept 3'}
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
